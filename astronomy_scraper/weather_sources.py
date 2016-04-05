eff3dd0c903729246360220be13c4ac8f97d1a44
Cameron A. Craig <camieac@gmail.com>
2016-04-05 22:52:28 +0100

    Add list of weather sources.

---
+++ astronomy_scraper/weather_sources.py
@@ -0,0 +1,7 @@
+sources = {
+    'new-york': {
+        'weather_url': 'http://www.timeanddate.com/weather/usa/new-york',
+        'sun_url': 'http://www.timeanddate.com/sun/usa/new-york?month=<month>&year=<year>',
+        'country': 'USA'
+    }
+}
