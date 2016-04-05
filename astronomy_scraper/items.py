c0ececd91652c4f35b279529e89433e34b8221f3
Cameron A. Craig <camieac@gmail.com>
2016-04-05 22:55:15 +0100

    Add sun and moon item fields.

--- astronomy_scraper/items.py
+++ astronomy_scraper/items.py
@@ -17,3 +17,28 @@ class AstronomyScraperItem(scrapy.Item):
 class DayConditions(scrapy.Item):
     moon_phase = scrapy.Field()
     temperature = scrapy.Field()
+    date = scrapy.Field()
+    sunrise = scrapy.Field()
+    sunset = scrapy.Field()
+    day_length = scrapy.Field()
+    day_difference = scrapy.Field()
+
+    # Astronomical Twilight
+    twil_astro_start = scrapy.Field()
+    twil_astro_end = scrapy.Field()
+
+    # Nautical Twilight
+    twil_naut_start = scrapy.Field()
+    twil_naut_end = scrapy.Field()
+
+    # Civil Twilight
+    twil_civil_start = scrapy.Field()
+    twil_civil_end = scrapy.Field()
+
+
+class MoonPhaseSnippet(scrapy.Item):
+    moon_phase_date = scrapy.Field()
+    moon_phase_time = scrapy.Field()
+    moon_phase_duration = scrapy.Field()
+
+    
